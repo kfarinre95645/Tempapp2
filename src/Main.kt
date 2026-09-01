22ddbc7f12ac4fa821d9181c227aa78aa49fd83f
Kenny Farinre <tur95645@temple.edu>
2026-09-01 12:18:54 -0400

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val myTemp = Temperature(Temperature.Unit.K)
+
+    myTemp.setTemp(0.0)
+
+    println("The temperature is ${myTemp.getTemp()}")
+}
